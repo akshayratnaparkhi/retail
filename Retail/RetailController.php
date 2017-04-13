4c6dce583f4b3780bd1c96ca3ea39a02475d88e8
nn730 <nn730@M4-A112335.ced.corp.cummins.com>
2017-04-12 22:38:28 -0400

    .htaccess added

--- Retail/RetailController.php
+++ Retail/RetailController.php
@@ -1,2 +1,22 @@
 <?php
-print ("I am controller");
+$view = "";
+if(isset($_GET["view"]))
+	$view = $_GET["view"];
+	/*
+	 controls the RESTful services
+	 URL mapping
+	 */
+	switch($view){
+		case "all":
+			print("All products printed");
+			break;
+		case "single":
+			print("product printed: "+$_GET("id"));
+			break;
+		case "hybrid":
+			print("product printed: "+$_GET("category")+" => "+$_GET("id"));
+			break;
+		default:
+			break;
+	}
+?>
